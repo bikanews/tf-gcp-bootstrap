274ef7506bb15e5a20187b8293090eb5f88a00eb
Beatriz Leite R. Santos <bikanews@gmail.com>
2024-05-17 10:41:58 -0300

    desenho final

--- main.tf
+++ main.tf
@@ -1,153 +1,146 @@
 provider "google" {
-  credentials = file("C:/Users/beatriz.leite.santos/Documents/Geneva-teste/credenciais/starry-strand-395614-04fdf7e501c4.json")
+  credentials = file("C:/Users/beatriz.leite.santos/Documents/Geneva-teste/credenciais/starry-strand-395614-0008357ef180.json")
   project     = "<starry-strand-395614>"
   region      = "us-central1"
 }
 
 
-### BOOTSTRAP ###
+## FOLDERS ##
 
 
-## Level 1 Folder ##
+## BOOTSTRAP - Level 1 Folder ##
 resource "google_folder" "bootstrap_folder" {
   display_name = "fldr-bootstrap"
   parent       = "organizations/510613106678"
 }
 
+## SHARED - Level 1 Folder ##
+resource "google_folder" "fldr-shared_folder" {
+  display_name = "fldr-shared"
+  parent       = "organizations/510613106678"
+}
+
+
+## Under SHARED - Level 2 Folders  ##
+resource "google_folder" "fldr_network" {
+  display_name = "fldr-network"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+resource "google_folder" "fldr-security" {
+  display_name = "fldr-security"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+resource "google_folder" "fldr-monitoring" {
+  display_name = "fldr-monitoring"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+resource "google_folder" "fldr-logs" {
+  display_name = "fldr-logs"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+resource "google_folder" "fldr-finops" {
+  display_name = "fldr-finops"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+
+resource "google_folder" "fldr-api-mgmt" {
+  display_name = "fldr-api-mgmt"
+  parent       = google_folder.fldr-shared_folder.name
+}
+
+## PROJECTS ##
+
+# Bootstrap projects #
 resource "google_project" "project_1" {
   name            = "eva-prj-iac-seed"
   project_id      = "eva-prj-iac-seed"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.bootstrap_folder.id
+  folder_id       = google_folder.bootstrap_folder.name
 }
 
 resource "google_project" "project_2" {
   name            = "eva-prj-iac-cicd"
   project_id      = "eva-prj-iac-cicd"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.bootstrap_folder.id
+  folder_id       = google_folder.bootstrap_folder.name
 }
 
-
-### NETWORKS ###
-
-
-## Level 2 Folders ##
-resource "google_folder" "network_folder" {
-  display_name = "fldr-network"
-  parent       = "google_folder.fldr-shared.id"
-}
-
+# Network projects #
 resource "google_project" "project_3" {
   name            = "eva-prj-hub-vpc"
-  project_id      = "eva-prj-hub-vpc"
+  project_id      = "id-eva-prj-hub-vpc"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.network_folder.id
+  folder_id       = google_folder.fldr_network.name
 }
 
 resource "google_project" "project_4" {
   name            = "eva-prj-dev-spk"
-  project_id      = "eva-prj-dev-spk"
+  project_id      = "id-eva-prj-dev-spk"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.network_folder.id
+  folder_id       = google_folder.fldr_network.name
 }
 
 resource "google_project" "project_5" {
   name            = "eva-prj-hml-spk"
-  project_id      = "eva-prj-hml-spk"
+  project_id      = "id-eva-prj-hml-spk"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.network_folder.id
+  folder_id       = google_folder.fldr_network.name
 }
 
 resource "google_project" "project_6" {
   name            = "eva-prj-prd-spk"
-  project_id      = "eva-prj-prd-spk"
+  project_id      = "id-eva-prj-prd-spk"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.network_folder.id
+  folder_id       = google_folder.fldr_network.name
 }
-
-
-### ORGANIZATION ###
-
-
-
-## Level 1 Folder ##
-resource "google_folder" "fldr-shared_folder" {
-  display_name = "fldr-shared"
-  parent       = "organizations/510613106678"
-}
-
-## Level 2 Folders ##
-resource "google_folder" "fldr-security" {
-  display_name = "fldr-security"
-  parent       = "google_folder.fldr-shared.id"
-}
-
-resource "google_folder" "fldr-monitoring" {
-  display_name = "fldr-monitoring"
-  parent       = "google_folder.fldr-shared.id"
-}
-
-resource "google_folder" "fldr-logs" {
-  display_name = "fldr-logs"
-  parent       = "google_folder.fldr-shared.id"
-}
-
-resource "google_folder" "fldr-finops" {
-  display_name = "fldr-finops"
-  parent       = "google_folder.fldr-shared.id"
-}
-
-
-resource "google_folder" "fldr-api-mgmt" {
-  display_name = "fldr-api-mgmt"
-  parent       = "google_folder.fldr-shared.id"
-}
-
-## Projects ## 
 
 # Security Projects #
 
 resource "google_project" "project_7" {
   name            = "eva-prj-security"
-  project_id      = "eva-prj-security"
+  project_id      = "id-eva-prj-security"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 resource "google_project" "project_8" {
   name            = "eva-prj-golden-images"
-  project_id      = "eva-prj-golden-images"
+  project_id      = "id-eva-prj-golden-images"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 resource "google_project" "project_9" {
   name            = "eva-prj-logs-audit"
-  project_id      = "eva-prj-logs-audit"
+  project_id      = "id-eva-prj-logs-audit"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 resource "google_project" "project_10" {
   name            = "eva-prj-logs-analysis"
-  project_id      = "eva-prj-logs-analysis"
+  project_id      = "id-eva-prj-logs-analysis"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 resource "google_project" "project_11" {
   name            = "eva-prj-logs-network"
-  project_id      = "eva-prj-logs-network"
+  project_id      = "id-eva-prj-logs-network"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 resource "google_project" "project_12" {
   name            = "eva-prj-sa"
-  project_id      = "eva-prj-sa"
+  project_id      = "id-eva-prj-sa"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-security.id
+  folder_id       = google_folder.fldr-security.name
 }
 
 
@@ -155,62 +148,62 @@ resource "google_project" "project_12" {
 
 resource "google_project" "project_13" {
   name            = "eva-prj-dev-monitoring"
-  project_id      = "eva-prj-dev-monitoring"
+  project_id      = "id-eva-prj-dev-monitoring"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-monitoring.id
+  folder_id       = google_folder.fldr-monitoring.name
 }
 
 resource "google_project" "project_14" {
   name            = "eva-prj-hml-monitoring"
-  project_id      = "eva-prj-hml-monitoring"
+  project_id      = "id-eva-prj-hml-monitoring"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-monitoring.id
+  folder_id       = google_folder.fldr-monitoring.name
 }
 
 resource "google_project" "project_15" {
   name            = "eva-prj-prd-monitoring"
-  project_id      = "eva-prj-prd-monitoring"
+  project_id      = "id-eva-prj-prd-monitoring"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-monitoring.id
+  folder_id       = google_folder.fldr-monitoring.name
 }
 
 # Logs Projects #
 
 resource "google_project" "project_16" {
   name            = "eva-prj-dev-logs"
-  project_id      = "eva-prj-dev-logs"
+  project_id      = "id-eva-prj-dev-logs"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-logs.id
+  folder_id       = google_folder.fldr-logs.name
 }
 
 resource "google_project" "project_17" {
   name            = "eva-prj-hml-logs"
-  project_id      = "eva-prj-hml-logs"
+  project_id      = "id-eva-prj-hml-logs"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-logs.id
+  folder_id       = google_folder.fldr-logs.name
 }
 
 resource "google_project" "project_18" {
   name            = "eva-prj-prd-logs"
-  project_id      = "eva-prj-prd-logs"
+  project_id      = "id-eva-prj-prd-logs"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-logs.id
+  folder_id       = google_folder.fldr-logs.name
 }
 
-# Logs Finops #
+# Finops Projects #
 
 resource "google_project" "project_19" {
   name            = "eva-prj-billing-export"
-  project_id      = "eva-prj-billing-export"
+  project_id      = "id-eva-prj-billing-export"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-finops.id
+  folder_id       = google_folder.fldr-finops.name
 }
 
-# Logs API Mgmt #
+# Logs API Mgmt Projects #
 
 resource "google_project" "project_20" {
   name            = "eva-prj-apigee"
   project_id      = "eva-prj-apigee"
   billing_account = "019A3B-184BE4-980D3A"
-  folder_id       = google_folder.fldr-api-mgmt.id
+  folder_id       = google_folder.fldr-api-mgmt.name
 }
